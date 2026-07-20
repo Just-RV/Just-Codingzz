2353cb61d13740cbf155c405d609551c64b9b960
Just-RV <ravipatel1233219@gmail.com>
2026-07-20 20:12:05 +0530

    four

--- Baisc-JS/Drill/First.js
+++ Baisc-JS/Drill/First.js
@@ -22,41 +22,41 @@ switch (gread) {
     break;
 }
 
-// const city = "surat"
+const city = "surat"
 
-// city = "vapi"
+city = "vapi"
 
-// let x // undefined
+let x // undefined
 
-// let x = "4"
+let x = "4"
 
-// let y = 4
+let y = 4
 
-// console.log(x == y)
-// console.log(x === y)
+console.log(x == y)
+console.log(x === y)
 
 
 
-// let gread = "A";
+let gread = "A";
 
-// switch (gread) {
-//   case "A":
-//     console.log("A Gread");
-//     break;
-//   case "B":
-//     console.log("B Gread");
-//     break;
-//   case "C":
-//     console.log("C Gread");
-//     break;
-//   case "D":
-//     console.log("D Gread");
+switch (gread) {
+  case "A":
+    console.log("A Gread");
+    break;
+  case "B":
+    console.log("B Gread");
+    break;
+  case "C":
+    console.log("C Gread");
+    break;
+  case "D":
+    console.log("D Gread");
 
-//   case "E":
-//     console.log("E Gread");
-//     break;
-//   case "F":
-//     console.log("F Gread");
-//     break;
-// }
+  case "E":
+    console.log("E Gread");
+    break;
+  case "F":
+    console.log("F Gread");
+    break;
+}
 
