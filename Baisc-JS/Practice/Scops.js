c18e0a4c9743d3d909844cae888df814eaed0302
Just-RV <ravipatel1233219@gmail.com>
2026-08-09 11:04:29 +0530

    Scops in JS

---
+++ Baisc-JS/Practice/Scops.js
@@ -0,0 +1,17 @@
+function Mul(){
+  let number = 4;
+
+ return function (){
+
+  number = number * number;
+
+  return number;
+ };
+
+}
+
+const num = Mul();
+console.log(num())
+console.log(num())
+
+
