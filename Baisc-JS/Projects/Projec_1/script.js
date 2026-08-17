d2b3427edc06fc23ee48232f35318ffcb3d98c65
Just-RV <ravipatel1233219@gmail.com>
2026-08-17 18:08:12 +0530

    First Project done(mini)

--- Baisc-JS/Projects/Projec_1/script.js
+++ Baisc-JS/Projects/Projec_1/script.js
@@ -1,1 +1,25 @@
-1
+console.log("Hii")
+
+const Bottons = document.querySelectorAll('.Botton');
+const body = document.querySelector('body');
+
+Bottons.forEach(function (Bottons){
+
+  Bottons.addEventListener('click' , function(e){
+
+  if(e.target.id === 'white'){
+    body.style.background = e.target.id;
+  }
+  if(e.target.id === 'red'){
+    body.style.background = e.target.id;
+  }
+  if(e.target.id === 'black'){
+    body.style.background = e.target.id;
+  }
+  if(e.target.id === 'yellow'){
+    body.style.background = e.target.id;
+  }
+
+  })
+
+})
